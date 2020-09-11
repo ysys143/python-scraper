1d33d14c321d4da7662eedb53bea9abf165e4498
JAESOL SHIN <ysys143@naver.com>
2020-09-11 17:27:13 +0900

    Update seoul corona.py

--- seoul corona.py
+++ seoul corona.py
@@ -1,69 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
 
 url = "https://www.seoul.go.kr/coronaV/coronaStatus.do"
 
-
-# In[3]:
-
-
 table = pd.read_html(url)
-table
-
-
-# In[4]:
-
+# table
 
 len(table)
-
-
-# In[5]:
-
-
+table[0]
 table[1]
-
-
-# In[6]:
-
-
 table[2]
-
-
-# In[7]:
-
-
 table[3]
 
-
-# In[8]:
-
-
-table[0]
-
-
-# In[11]:
-
-
 table[3].to_csv("서울코로나.csv", encoding="cp94", index=False)
-
-
-# In[10]:
-
-
 pd.read_csv("서울코로나.csv", encoding="cp94")
-
-
-# In[ ]:
-
-
-
-
