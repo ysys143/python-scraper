d289e6e3964985095b2b6e238148be06fa2a5118
JAESOL SHIN <ysys143@naver.com>
2020-09-11 18:14:49 +0900

    Update 정보공개포털 - 선불식 할부거래.py

--- 정보공개포털 - 선불식 할부거래.py
+++ 정보공개포털 - 선불식 할부거래.py
@@ -1,8 +1,14 @@
 import requests
 
-baseURL = "https://www.open.go.kr/othicInfo/infoList/infoList.ajax"
-query = "kwd=%EC%84%A0%EB%B6%88%EC%8B%9D%ED%95%A0%EB%B6%80%EA%B1%B0%EB%9E%98&insttSeCd=&eduYn=N&startDate=20100812&endDate=20200910&insttCdNm=&insttCd=&searchMainYn=&viewPage=1&rowPage=1000&sort=s"
-url = f"{baseURL}?{query}"
+startDate = "20100812"
+endDate = "20200910"
+rowPage = "1000"
+
+baseURL = "https://www.open.go.kr/othicInfo/infoList/infoList.ajax?"
+query1 = "kwd=%EC%84%A0%EB%B6%88%EC%8B%9D%ED%95%A0%EB%B6%80%EA%B1%B0%EB%9E%98&insttSeCd=&eduYn=N&" 
+query2 = f"startDate={startDate}&endDate={endDate}&" 
+query3 = f"insttCdNm=&insttCd=&searchMainYn=&viewPage=1&rowPage={rowPage}&sort=s"
+url = baseURL+query1+query2+query3
 
 
 response = requests.post(url)
